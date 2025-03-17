e65cfe6057844b2e9c32f01cbf70e5822838a546
poupuu <williamanwar2@gmail.com>
2025-03-17 14:30:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,11 @@ def main():
                thinning_suprapapillary_epidermis, spongiform_pustule, munro_microabcess, focal_hypergranulosis, disappearance_granular_layer,
                vacuolisation_damage_basal_layer, spongiosis, saw_tooth_appearance_retes, follicular_horn_plug, perifollicular_parakeratosis,
                inflammatory_mononuclear_infiltrate, band_like_infiltrate, age]
+  model_filename = 'trained_model.pkl'
+  model = load_model(model_filename)
+  prediction = predict_with_model(model, user_input)
+  st.write('The prediction output is: ', prediction)
+
 if __name__ == "__main__":
 	main()
 
