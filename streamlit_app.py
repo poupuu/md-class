0a59694887270b0b4ed151a6785f2d345847b61f
poupuu <williamanwar2@gmail.com>
2025-03-17 14:11:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import joblib
+def main():
+  st.title('Dermatology Machine Learning')
+  st.write('Halo!')
+  
+if __name__ == "__main__":
+	main()
 
-st.title('Dermatology Machine Learning')
-
-st.write('Halo!')
