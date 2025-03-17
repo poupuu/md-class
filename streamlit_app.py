eed53c904ebd6ae69a21b27639dd7b3026a41910
poupuu <williamanwar2@gmail.com>
2025-03-17 14:35:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ def load_model(filename):
   model = joblib.load(filename)
   return model
 
-def predict_with_moel(model, user_input):
+def predict_with_model(model, user_input):
   prediction = model.predict([user_input])
   return prediction[0]
 
