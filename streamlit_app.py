c2c2e55fec0b8f26b994b633d7112429862ca5c1
poupuu <williamanwar2@gmail.com>
2025-03-17 14:17:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import joblib
+
 def main():
   st.title('Dermatology Machine Learning')
   st.write('Halo!')
+  
+  #input data by user
+  erythema = st.slider('erythema', min_value = 0, max_value=3, value=2)
+  #min_value dan max_value = liat dari data, dan value itu nilai default pas dideploy
+
   
 if __name__ == "__main__":
 	main()
